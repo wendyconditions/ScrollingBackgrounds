3f0d8dc99283514a28fbdf43f193b844bcd2c8a9
Wendy Conditions <wam922@gmail.com>
2017-02-09 11:36:42 -0800

    Update main.js

--- main.js
+++ main.js
@@ -1,12 +1,3 @@
- /* 
-See http://codepen.io/MarcelSchulz/full/lCvwq
-
-The effect doens't appear as nice when viewing in split view :-)
-
-Fully working version can also be found at (http://schulzmarcel.de/x/drafts/parallax).
-
-*/
-
 jQuery(document).ready(function(){
   $(window).scroll(function(e){
   	parallaxScroll();
